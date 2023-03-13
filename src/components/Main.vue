5dfb201d14d2d588724b43f636a2a60d01b9774a
Mikkel <Mikkelmbk@live.dk>
2023-03-13 19:34:04 +0100

    Fetching data from JsonPlaceholder API, and handling it in Products.vue, where we v-for throug the products and prop drill the necessary data values to Product.vue. Next step is either bringing in the Modal or setting up the swiper-slider integration

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,16 @@
 <script setup>
-
+    import Products from "./Products.vue"
 </script>
 
 <template>
     <main class="c-main h-nav-spacing">
 
-        <div class="l-grid"></div>
+        <div class="js-swiper-anchor">
+        </div>
+
+        <div class="l-grid js-product-anchor">
+            <Products/>
+        </div>
     </main>
 </template>
 
---
+++ src/components/Product.vue
@@ -0,0 +1,65 @@
+<script setup>
+    import { defineProps } from "vue";
+
+    const { title, image } = defineProps(['title','image']);
+</script>
+
+<template>
+    <div class="c-product js-product">
+        <figure class="c-product__figure">
+            <img :src="image" :alt="title" class="c-product__image">
+            <figcaption class="c-product__figcaption">{{ title }}</figcaption>
+        </figure>
+        <section class="c-product__content"></section>
+    </div>
+</template>
+
+<style scoped>
+
+.c-product{
+    background-color: var(--primary-theme-light-grey);
+    padding:10px;
+    transition: all .3s ease;
+    height:100%;
+}
+
+.c-product:hover{
+    transform:scale(1.02);
+    box-shadow: 0px 0px 10px 3px rgba(0,0,0,0.75);
+    cursor:pointer;
+}
+
+.c-product__figure{
+    text-align: center;
+}
+
+.c-product__image{
+    width: 100%;
+    display: block;
+}
+
+.c-product__figcaption{
+    color: var(--primary-theme-transparent-text);
+    padding: 15px 0;
+    font-weight:bold;
+}
+
+.c-product--modal{
+    height:auto;
+    width:80%;
+    max-width:450px;
+}
+
+.c-product--modal:hover{
+    transform:unset;
+    box-shadow:unset;
+    cursor:unset;
+}
+
+@media(min-width:768px){
+    .c-product--modal{
+        max-width: 525px;
+    }
+}
+
+</style>
