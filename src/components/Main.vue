7ce7202fa2f774e392b007dddfd2e95a4b69745f
Mikkel <Mikkelmbk@live.dk>
2023-03-15 22:33:24 +0100

    Refactored Pinia products store such that the state handling of which sliders, and how many sliders, are shown on a given route. The Pinia products store are now in control of handing over the productFetchConfig data to the useFetchProducts composable that returns the respective product data to GridProducts.vue and SwiperProducts.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,19 @@
 import { ref } from "vue";
 import GridProducts from "./GridProducts.vue"
 import SwiperProducts from "./SwiperProducts.vue"
+import { useProductsStore } from "../stores/products"
+import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router"
-
 const router = useRouter();
 const currentPath = ref(router.currentRoute.value.path);
+
+const productsStore = useProductsStore();
+
+const { productFetchConfig } = storeToRefs(productsStore);
+
+const { updateFetchConfig } = productsStore;
+
+updateFetchConfig(currentPath.value);
 
 function loadGridProducts() {
     return currentPath.value == "/" || currentPath.value == "/contact" ? false : true;
@@ -21,11 +30,10 @@ function loadSwiperProducts() {
     <main class="c-main h-nav-spacing">
 
         <div v-if="loadSwiperProducts()" class="js-swiper-anchor">
-            <SwiperProducts :currentPath="currentPath" />
+            <SwiperProducts v-for="config in productFetchConfig" :productFetchUrl="config.productFetchUrl" :start="config.start" :end="config.end" :swiperTitle="config.swiperTitle" />
         </div>
-
         <div v-if="loadGridProducts()" class="l-grid js-product-anchor">
-            <GridProducts :currentPath="currentPath"/>
+            <GridProducts v-for="config in productFetchConfig" :productFetchUrl="config.productFetchUrl" :start="config.start" :end="config.end"/>
         </div>
     </main>
 </template>
