385d6ada27ba99adb35c96ca44999b1da1f909ac
Mikkel <Mikkelmbk@live.dk>
2023-03-16 18:15:48 +0100

    added Modal.vue component and refactored useRouter so that it exists in Wrapper.vue as opposed to Main.vue. Main.vue is now receiving the currentPath variable as a prop passed from Wrapper.vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Product from "./Product.vue";
+</script>
+
+<template>
+    <div class="c-modal">
+        <Product :modalClass="`c-product--modal`" :title="`Some title`" :image="`https://via.placeholder.com/600/92c952`"/>
+    </div>
+</template>
+
+<style scoped>
+.c-modal{
+    position:fixed;
+    inset:0;
+    top:calc(var(--primary-mobile-navigation-height) + var(--primary-mobile-header-height));
+    background-color: rgba(0, 0, 0, 0.8);
+    display:flex;
+    justify-content:center;
+    align-items:center;
+}
+
+@media(min-width:768px){
+    .c-modal{
+        top: var(--primary-desktop-header-height);
+    }
+}
+</style>
